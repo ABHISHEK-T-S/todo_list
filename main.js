313243a425745794ffb60d18798df2222a054fb0
Abhishek T S <tsabhishek1999@gmail.com>
2020-06-22 20:53:51 +0530

    js change *

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function addtask(){
-    var task = document.getElementById("task");
-    var time =document.getElementById("time");
-    var z =document.getElementById("tasklist");
-    z.innerHTML="task  :"+task+"</br> time :"+time;
+
+    document.getElementById("tasklist").innerHTML="helloworld";
+   
+
 }
