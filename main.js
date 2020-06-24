7892807a26d59707ee848967df589b8198081793
Abhishek T S <tsabhishek1999@gmail.com>
2020-06-24 09:44:52 +0530

    task added sucessfully

--- main.js
+++ main.js
@@ -1,47 +1,65 @@
-var str1='{}';
-var limit;
 
-
-function addtask(){
+function addtask() {
 
     var z = document.getElementById("tasklist");
-    
+
     var task = document.getElementById("task").value;
-    
+
     var time = document.getElementById("time").value;
-    
+
     var date = document.getElementById("date").value;
-   
-    str1 =str1+',{"task":"'+task+'" ,"date":"'+date+'","time":"'+time+'"}';
+
+    var parsedObject = JSON.parse(retrievedObject);
+    //str1 = str1 + ',{"task":"' + task + '" ,"date":"' + date + '","time":"' + time + '"}';
+    var str1 = '{"task":"' + task + '" ,"date":"' + date + '","time":"' + time + '"},{}';
+    retrievedObject=retrievedObject.slice(0,-4);
+    if (parsedObject.length>=1){
+    retrievedObject = retrievedObject+',' + str1 + ']}';
+    }
+    else{
+        retrievedObject = retrievedObject + str1 + ']}';
+    }
+    console.log(retrievedObject);
+
     
-    var string = '{"taskitems":['+str1+']}';
-    console.log(string);
 
     // DO NOT STRINGIFY AGAIN WHEN WRITING TO LOCAL STORAGE
-    localStorage.setItem('added-items', string);
-    
+    localStorage.setItem('added-items', retrievedObject);
+
     // READ STRING FROM LOCAL STORAGE
-    var retrievedObject = localStorage.getItem('added-items');
-    
+    var retrievedObject2= localStorage.getItem('added-items');
+
+
     // CONVERT STRING TO REGULAR JS OBJECT
-    var parsedObject = JSON.parse(retrievedObject);
-    limit=parsedObject.taskitems.length;
-    
+    var parsedObject = JSON.parse(retrievedObject2);
+    limit = parsedObject.taskitems.length;
+
     // ACCESS DATA
-    for(var i=1 ;i<limit;i++){
-        console.log(parsedObject.taskitems[i].task);
-        z.innerHTML=z.innerHTML+'<div class ="tasks" id = "tasks">task : '+parsedObject.taskitems[i].task+'<br> date : '+parsedObject.taskitems[i].date+'<br>  time : '+parsedObject.taskitems[i].time+'</div>';
-    }
-    
+
+        console.log(parsedObject.taskitems[limit-2].task);
+        z.innerHTML = z.innerHTML + '<div class ="tasks" id = "tasks">task : ' + parsedObject.taskitems[limit-2].task + '<br> date : ' + parsedObject.taskitems[limit-2].date + '<br>  time : ' + parsedObject.taskitems[limit-2].time + '</div>';
+
+
 }
+
+var limit;
+var z = document.getElementById("tasklist");
+
 var retrievedObject = localStorage.getItem('added-items');
-    
-// CONVERT STRING TO REGULAR JS OBJECT
 var parsedObject = JSON.parse(retrievedObject);
-limit=parsedObject.taskitems.length;
-for(var i=1 ;i<limit;i++){
-    var z = document.getElementById("tasklist");
-    console.log(parsedObject.taskitems[i].task);
-    z.innerHTML=z.innerHTML+'<div class ="tasks" id = "tasks">task : '+parsedObject.taskitems[i].task+'<br> date : '+parsedObject.taskitems[i].date+'<br>  time : '+parsedObject.taskitems[i].time+'</div>';
+
+if (retrievedObject == null) {
+    retrievedObject = '{"taskitems":['+'{}]}';
+}
+else {
+    // CONVERT STRING TO REGULAR JS OBJECT
+    console.log(retrievedObject);
+    var parsedObject = JSON.parse(retrievedObject);
+    limit = parsedObject.taskitems.length;
+    for (var i = 0; i < limit-1; i++) {
+        
+        //console.log(parsedObject.taskitems[i].task);
+        z.innerHTML = z.innerHTML + '<div class ="tasks" id = "tasks">task : ' + parsedObject.taskitems[i].task + '<br> date : ' + parsedObject.taskitems[i].date + '<br>  time : ' + parsedObject.taskitems[i].time + '</div>';
+    }
 }
 
